ee47df642c13ca6309f1be712218872840d1e1fb
RM35 <92856544+RM35@users.noreply.github.com>
2022-07-02 13:55:38 +0100

    feat: Update player

--- scenes/world/TM.gd
+++ scenes/world/TM.gd
@@ -2,7 +2,7 @@ extends TileMap
 
 const x_view = 40
 const y_view = 24
-onready var player = get_node("../Player2D")
+onready var player = get_node("../Player")
 
 func _process(delta):
 	render_tiles()
@@ -10,8 +10,8 @@ func _process(delta):
 func render_tiles():
 	clear()
 	var nearest_cell = world_to_map(player.global_position) - Vector2(x_view / 2, y_view / 2)
-	for i in range(x_tiles_view_range):
+	for i in range(x_view):
 		var nextc = ((i * Vector2.RIGHT) + nearest_cell)
-		for j in range(y_tiles_view_range):
+		for j in range(y_view):
 			nextc = Vector2.DOWN + nextc
 			set_cellv(nextc, 0)	
