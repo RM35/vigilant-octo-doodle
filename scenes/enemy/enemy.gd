af93d75d3033b4878080e3edd317b124d561e363
RM35 <92856544+RM35@users.noreply.github.com>
2022-07-06 00:09:52 +0100

    feat(spawner): Add spawner

--- scenes/enemy/enemy.gd
+++ scenes/enemy/enemy.gd
@@ -1,13 +1,19 @@
 extends KinematicBody2D
 
+class_name enemy
+
 onready var player = get_node("../Player")
 export var u_data: Resource = load("res://scenes/enemy/enemy_data/carrot.tres")
 var velocity = Vector2.ZERO
 var knock_back = false
 
 func _ready():
-	$Sprite.texture = u_data.texture
+	set_enemy_type("res://scenes/enemy/enemy_data/carrot.tres")
 
+func set_enemy_type(unit_data: String):
+	u_data = load(unit_data)
+	$Sprite.texture = u_data.texture
+	
 func _physics_process(delta):
 	velocity = Vector2.ZERO
 	velocity = global_position.direction_to(player.position) * u_data.speed
