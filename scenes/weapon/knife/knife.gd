0c1f82fc100caa4d42aa2f121b4645d2d88a126d
RM35 <92856544+RM35@users.noreply.github.com>
2022-07-10 22:52:20 +0100

    fix(knife): Change knife from disctance to time reset

--- scenes/weapon/knife/knife.gd
+++ scenes/weapon/knife/knife.gd
@@ -10,6 +10,7 @@ var dir: Vector2 = Vector2.RIGHT
 var origin_vector_global = Vector2.ZERO
 var last_dir = Vector2.RIGHT
 var player_prev_pos = Vector2.ZERO
+var reset = false
 
 # Upgradable
 onready var damage = get_parent().damage
@@ -17,6 +18,7 @@ onready var speed = get_parent().speed
 
 func _ready():
 	player_prev_pos = player.global_position
+	$Timer.start()
 	
 func get_input_axis():
 	var axis = Vector2.ZERO
@@ -28,10 +30,11 @@ func _process(delta):
 	var axis = get_input_axis()
 	if axis != Vector2.ZERO:
 		last_dir = axis
-	if self.global_position.distance_to(player.global_position) <= 400:
+	if !reset:
 		var offset = player_prev_pos - player.global_position
 		global_position = global_position  + (dir * speed * delta) + offset
 	else:
+		reset = false
 		dir = last_dir
 		if dir.x < 0:
 			$Sprite.set_scale(Vector2(-1,1))
@@ -44,3 +47,6 @@ func _process(delta):
 func _on_Area2D_body_entered(body):
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
+
+func _on_Timer_timeout():
+	reset = true
