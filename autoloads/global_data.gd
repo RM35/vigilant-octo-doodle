ee61d0b2df0b17cf33e0d71bf5eee8677d771d7a
RM35 <92856544+RM35@users.noreply.github.com>
2022-07-12 14:07:41 +0100

    feat: Add time survived to metrics

--- autoloads/global_data.gd
+++ autoloads/global_data.gd
@@ -5,3 +5,4 @@ enum STATE {MAIN, PAUSE, PLAY}
 
 var selected_level = "massive_test"
 var score =  22
+var time_survived = 0
--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -4,5 +4,6 @@ var time = 0
 
 func _process(delta):
 	time += delta
+	GlobalData.time_survived = time
 	$Score.text = str("%3.0f" % GlobalData.score)
-	$Time.text = str("%3.0f" % time)
+	$Time.text = str("%3.0f" % GlobalData.time)
